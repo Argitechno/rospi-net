1914c7d650125ff5660ed42be6726fcb8ac95769
Caleb Blondell <caleb.blondell@gmail.com>
2025-06-18 10:35:09 -0700

    feat: add timing support to parameters, with a default publish rate

--- ros2_ws/src/netstress/netstress/router.py
+++ ros2_ws/src/netstress/netstress/router.py
@@ -8,7 +8,6 @@ from netstress.counting_subscriber import CountingSubscriber
 class Router(Node):
     def __init__(self):
         super().__init__('router')
-
        
         # Declare parameters with default empty lists
         self.declare_parameter(
@@ -22,11 +21,28 @@ class Router(Node):
             value=[''],
             descriptor=ParameterDescriptor(type=ParameterType.PARAMETER_STRING_ARRAY)
         )
+        
+        default_publish_rate = 3.0
+        
+        self.declare_parameter(
+            'default_publish_rate',
+            value=default_publish_rate
+        )
+        
+        self.declare_parameter(
+            'publish_rates',
+            value=[default_publish_rate],
+            descriptor=ParameterDescriptor(type=ParameterType.PARAMETER_DOUBLE_ARRAY)
+        )
+        
 
         # Get parameter values as string arrays
         self.publish_topics = [t for t in self.get_parameter_or('publish_topics', Parameter('publish_topics', Parameter.Type.STRING_ARRAY, [''])).get_parameter_value().string_array_value if t]
         self.subscribe_topics = [t for t in self.get_parameter_or('subscribe_topics', Parameter('subscribe_topics', Parameter.Type.STRING_ARRAY, [''])).get_parameter_value().string_array_value if t]
-
+        
+        self.default_publish_rate = self.get_parameter('default_publish_rate').get_parameter_value().double_value
+        self.publish_rates = [t for t in self.get_parameter_or('publish_rates', Parameter('publish_rates', Parameter.Type.DOUBLE_ARRAY, [self.default_publish_rate])).get_parameter_value().double_array_value if t]
+        
         self.get_logger().info(f"Publish topics: {self.publish_topics}")
         self.get_logger().info(f"Subscribe topics: {self.subscribe_topics}")
 
@@ -35,10 +51,19 @@ class Router(Node):
         if not self.subscribe_topics:
             self.get_logger().warn("No valid subscribe_topics specified.")
 
+         # Match rates to topics
+        self.publish_params = []
+        for idx, topic in enumerate(self.publish_topics):
+            if idx < len(self.publish_rates):
+                rate = self.publish_rates[idx]
+            else:
+                rate = self.default_publish_rate
+            self.publish_params.append((topic, rate))
+
         # Create CountingPublisher instances for each publish topic
         self.counting_publishers = {}
-        for topic in self.publish_topics:
-            self.counting_publishers[topic] = CountingPublisher(self, topic)
+        for topic, rate in self.publish_params:
+            self.counting_publishers[topic] = CountingPublisher(self, topic, rate)
 
         # Create CountingSubscriber instances for each subscribe topic
         self.counting_subscribers = {}
